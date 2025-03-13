cc9bf5c79d75cc81cb1b0920d6d8d064bc499c31
odedh <odedh@neureality.ai>
2025-03-13 16:03:11 +0000

    Switched from vLLM implementation to HF implementation (siglip and resampler)

---
+++ constants.py
@@ -0,0 +1,5 @@
+SIGLIP_CONFIG_FILE_PATH = "siglip/config.json"
+RESAMPLER_CONFIG_FILE_PATH = "resampler/config.json"
+
+SIGLIP_STATE_DICT_PATH = "siglip/weights/siglip_weights.pth"
+RESAMPLER_STATE_DICT_PATH = "resampler/weights/resampler_weights.pth"
--- vfm.py
+++ vfm.py
@@ -1,31 +1,82 @@
-from torch import nn
 import torch
-from resampler.resampler import Resampler2_5
-from siglip.idefics2_vision_model import Idefics2VisionTransformer
-from vllm.model_executor.model_loader.utils import set_default_torch_dtype
+from torch import nn
+from transformers import AutoConfig
+from constants import *
+
+from siglip.modeling_navit_siglip import SiglipVisionTransformer
+from resampler.resampler import Resampler
 
 
 class VFM(nn.Module):
-    def __init__(self, siglip_config, resampler_config):
+    def __init__(
+        self,
+        siglip_config: str = SIGLIP_CONFIG_FILE_PATH,
+        resampler_config: str = RESAMPLER_CONFIG_FILE_PATH,
+        dtype: torch.dtype = torch.bfloat16,
+        device: torch.device = torch.device("cuda"),
+    ):
         super().__init__()
-        self.siglip_config = siglip_config
-        self.resampler_config = resampler_config
+        self.device = device
+        self.dtype = dtype
+        self.siglip_config = AutoConfig.from_pretrained(siglip_config)
+        self.resampler_config = AutoConfig.from_pretrained(resampler_config)
         self.resampler = self.init_resampler()
         self.vpm = self.init_vision_module()
 
+        self._load_model_weights()
+
     def init_vision_module(self) -> nn.Module:
-        model = Idefics2VisionTransformer(self.siglip_config)
+        # same as HuggingFaceM4/siglip-so400m-14-980-flash-attn2-navit add tgt_sizes
+        if self.siglip_config._attn_implementation == "flash_attention_2":
+            self.siglip_config._attn_implementation = "flash_attention_2"
+        else:
+            # not suport sdpa
+            self.siglip_config._attn_implementation = "eager"
+        model = SiglipVisionTransformer(self.siglip_config)
         if self.siglip_config.drop_vision_last_layer:
             model.encoder.layers = model.encoder.layers[:-1]
+
+        setattr(model, "embed_dim", model.embeddings.embed_dim)
+        setattr(model, "patch_size", model.embeddings.patch_size)
+
         return model
 
-    def init_resampler(self, embed_dim: int, vision_dim: int) -> nn.Module:
-        with set_default_torch_dtype(torch.float16):
-            # The resampler in 2.6 remains consistent with the one in 2.5.
-            resampler = Resampler2_5(
-                num_queries=self.resampler_config.query_num,
-                embed_dim=self.resampler_config.embed_dim,
-                num_heads=self.resampler_config.embed_dim // 128,
-                kv_dim=self.resampler_config.vision_dim,
-            )
+    def init_resampler(self) -> nn.Module:
+        # The resampler in 2.6 remains consistent with the one in 2.5.
+        resampler = Resampler(
+            num_queries=self.resampler_config.query_num,
+            embed_dim=self.resampler_config.embed_dim,
+            num_heads=self.resampler_config.embed_dim // 128,
+            kv_dim=self.resampler_config.vision_dim,
+            adaptive=True,
+        )
         return resampler
+
+    def _load_model_weights(
+        self,
+        resampler_state_dict_path: str = RESAMPLER_STATE_DICT_PATH,
+        vpm_state_dict_path: str = SIGLIP_STATE_DICT_PATH,
+    ) -> None:
+        self.resampler.load_state_dict(
+            torch.load(resampler_state_dict_path, weights_only=True)
+        )
+        self.vpm.load_state_dict(torch.load(vpm_state_dict_path, weights_only=True))
+
+    def forward(
+        self,
+        all_pixel_values: torch.Tensor,
+        patch_attn_mask: torch.Tensor,
+        tgt_sizes: torch.Tensor,
+    ) -> torch.Tensor:
+        if all_pixel_values.dtype != self.dtype:
+            print(
+                f"Current dtype of all_pixel_values is {all_pixel_values.dtype}, Converting all_pixel_values to {self.dtype}"
+            )
+            all_pixel_values = all_pixel_values.to(self.dtype)
+
+        vision_embedding = self.vpm(
+            all_pixel_values, patch_attention_mask=patch_attn_mask, tgt_sizes=tgt_sizes
+        ).last_hidden_state
+        vision_embedding = self.resampler(vision_embedding, tgt_sizes)
+
+        return vision_embedding
